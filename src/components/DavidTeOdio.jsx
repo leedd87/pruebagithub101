c2906547b9c2549784f66ac78a9ddc8741bdca81
leedd87 <leedav87@gmail.com>
2022-07-17 23:25:57 -0300

    limpieza de componentes

--- src/components/DavidTeOdio.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const DavidTeOdio = () => {
-  return (
-    <div>DavidTeOdio</div>
-  )
-}
--- src/components/componenteLaura.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-
-export default function ComponenteLaura(){
-    return(
-        <h1>HOLAAA SOY LAURAA</h1>
-    )
-}
