6cf239762211158dd5ea3aa1cad8aef02a5c7747
ivan gauto <ivaangauto@gmail.com>
2022-07-17 23:50:07 -0300

    guadanomemates

--- src/components/DavidNoMeElegiste.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const DavidNoMeElegiste = () => {
-  return (
-    <div>David_No_Me_Elegiste_Malvado</div>,
-    <div>Sin_Corazon</div>
-  )
-}
--- src/components/holaConPunto.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function holaConPunto() {
-  return (
-    <div>holaConPunto</div>
-  )
-}
